21e7832496330816c1d976bbce2a5c23ca46923f
Tikiram Ruiz <tikiram@icloud.com>
2025-05-06 14:38:18 -0600

    fix: name fix

--- Sources/DynamoUtils/to_dynamo_value.swift
+++ Sources/DynamoUtils/to_dynamo_value.swift
@@ -1,35 +1,35 @@
 import AWSDynamoDB
 import Foundation
 
-func toDynamoValue(_ string: String?) -> DynamoDBClientTypes.AttributeValue {
+public func toDynamoValue(_ string: String?) -> DynamoDBClientTypes.AttributeValue {
   guard let string else {
     return .null(true)
   }
   return .s(string)
 }
 
-func toDynamoValue(_ bool: Bool?) -> DynamoDBClientTypes.AttributeValue {
+public func toDynamoValue(_ bool: Bool?) -> DynamoDBClientTypes.AttributeValue {
   guard let bool else {
     return .null(true)
   }
   return .bool(bool)
 }
 
-func toDynamoValue(_ int: Int?) -> DynamoDBClientTypes.AttributeValue {
+public func toDynamoValue(_ int: Int?) -> DynamoDBClientTypes.AttributeValue {
   guard let int else {
     return .null(true)
   }
   return .n(int.description)
 }
 
-func toDynamoValue(_ value: Double?) -> DynamoDBClientTypes.AttributeValue {
+public func toDynamoValue(_ value: Double?) -> DynamoDBClientTypes.AttributeValue {
   guard let value else {
     return .null(true)
   }
   return .n(value.description)
 }
 
-func toDynamoValue(_ date: Date?) -> DynamoDBClientTypes.AttributeValue {
+public func toDynamoValue(_ date: Date?) -> DynamoDBClientTypes.AttributeValue {
   guard let date else {
     return .null(true)
   }
